b8731a2e0718929f9516e3007e589d243085dfe1
arthurlustosa <arthur.souza@ccc.ufcg.edu.br>
2015-11-02 22:26:05 -0300

    Testes prontos, erro import play.db.jpa.JPAPlugin

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,28 +1,84 @@
-import org.junit.*;
 
-import play.mvc.*;
-import play.test.*;
-import play.libs.F.*;
 
-import static play.test.Helpers.*;
-import static org.junit.Assert.*;
+import models.Anuncio;
+import models.dao.GenericDAO;
+import play.db.jpa.Transactional;
+import play.test.FakeApplication;
+import org.junit.Test;
+import play.db.jpa.JPA;
+import play.test.WithBrowser;
+import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.inMemoryDatabase;
 
-import static org.fluentlenium.core.filter.FilterConstructor.*;
+public class IntegrationTest extends WithBrowser {
 
-public class IntegrationTest {
-
-    /**
-     * add your integration test here
-     * in this example we just check if the welcome page is being shown
-     */
-    @Test
-    public void test() {
-        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
-            public void invoke(TestBrowser browser) {
-                browser.goTo("http://localhost:3333");
-                assertTrue(browser.pageSource().contains("Your new application is ready."));
-            }
-        });
+    @Override
+    public FakeApplication provideFakeApplication() {
+        return fakeApplication(inMemoryDatabase());
     }
 
+    @Test
+    public void deveCarregarPaginaPrincipal() {
+        browser.goTo("http://localhost:" + testServer.port());
+        assertThat(browser.pageSource()).contains("Fun Music ADS");
+        assertThat(browser.pageSource()).contains("0 Anúncios finalizados");
+    }
+
+    @Test
+    public void deveAdicionarAnuncio() {
+        browser.goTo("http://localhost:" + testServer.port());
+        assertThat(browser.pageSource()).doesNotContain("Testando pelo código");
+        browser.click("#botaoAnunciar");
+
+        browser.fill("#inputTitulo").with("Testando pelo código");
+        browser.fill("#inputDescricao").with("Anúncio criado nos testes");
+        browser.fill("#inputCidade").with("Campina Grande");
+        browser.fill("#inputBairro").with("Bodocongó");
+        browser.fill("#inputInstrumentos").with("Gaita, Violão, Saxofone");
+        browser.fill("#inputEstilos").with("Blues, Rock, Jazz");
+        browser.fill("#inputEstilosBanidos").with("Pagode, Funk");
+        browser.fill("#inputContatos").with("Contato dos testes");
+        browser.fill("#inputCodigo").with("testando123");
+        browser.fill("#inputContatos").with("Contato dos testes");
+        browser.fillSelect("#inputContatos1");
+
+        browser.click("#submitNovoAnuncio");
+        assertThat(browser.pageSource()).contains("Testando pelo código");
+    }
+
+    @Test
+    public void deveFinalizarAnuncio() {
+        deveAdicionarAnuncio();
+
+        browser.goTo("http://localhost:" + testServer.port());
+        assertThat(browser.pageSource()).contains("Testando pelo código");
+
+        browser.click(".finalizarAnuncio");
+        browser.fill("#inputFinalizar").with("testando123");
+        browser.fillSelect("#inputEncontrou1");
+        browser.click("#submitFinalizar26");
+
+        assertThat(browser.pageSource()).doesNotContain("Testando pelo código");
+    }
+
+    @Test
+    public void deveFazerPerguntaEResposta() {
+        deveAdicionarAnuncio();
+
+        browser.goTo("http://localhost:" + testServer.port());
+        assertThat(browser.pageSource()).contains("Testando pelo código");
+        assertThat(browser.pageSource()).doesNotContain("Testando pergunta pelo código");
+
+        browser.fill("#formFazerPergunta26").with("Testando pergunta pelo código");
+        browser.click("#submitFormFazerPergunta26");
+        assertThat(browser.pageSource()).contains("Testando pergunta pelo código");
+        assertThat(browser.pageSource()).doesNotContain("Testando resposta pelo código");
+
+        browser.click("#botaoResponderPergunta26");
+        browser.fill("#inputCodigo").with("testando123");
+        browser.fill("#inputResposta").with("Testando resposta pelo código");
+        browser.click("#submitResponderPergunta");
+        assertThat(browser.pageSource()).contains("Testando resposta pelo código");
+    }
 }
---
+++ test/funcional/ApplicationControllerTest.java
@@ -0,0 +1,132 @@
+package funcional;
+
+import controllers.Application;
+import models.Anuncio;
+import models.Chat;
+import models.dao.GenericDAO;
+import org.junit.*;
+import play.db.jpa.JPA;
+import play.db.jpa.JPAPlugin;
+import play.mvc.Result;
+import play.test.FakeApplication;
+import play.test.FakeRequest;
+import play.test.Helpers;
+import scala.Option;
+
+
+import javax.persistence.EntityManager;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.*;
+
+public class ApplicationTest {
+    private static FakeApplication app;
+    public EntityManager em;
+    private GenericDAO DAO = new GenericDAO();
+
+    @BeforeClass
+    public static void startApp() {
+        app = Helpers.fakeApplication(new Global());
+        Helpers.start(app);
+    }
+
+    @AfterClass
+    public static void stopApp() {
+        Helpers.stop(app);
+    }
+
+    @Before
+    public void setUp() {
+        Option<JPAPlugin> jpaPlugin = app.getWrappedApplication().plugin(JPAPlugin.class);
+        em = jpaPlugin.get().em("default");
+        JPA.bindForCurrentThread(em);
+        em.getTransaction().begin();
+    }
+
+    @Test
+    public void deveCarregarIndexOk() {
+        Result result = Application.index();
+        assertThat(status(result)).isEqualTo(OK);
+    }
+
+    @Test
+    public void deveCarregarAnunciosOk() {
+        Result result = callAction(controllers.routes.ref.Application.anuncios(), new FakeRequest());
+        assertThat(status(result)).isEqualTo(OK);
+    }
+
+    @Test
+    public void deveCriarAnuncio() {
+        Map<String, String> requestMap = new HashMap<>();
+        requestMap.put("titulo", "Testando");
+        requestMap.put("descricao", "Só uma descrição de teste...");
+        requestMap.put("cidade", "Mãe D'água");
+        requestMap.put("bairro", "Centro");
+        requestMap.put("instrumentos", "Violão e Gaita");
+        requestMap.put("estilos", "Forró pé de serrá");
+        requestMap.put("estilosBanidos", "Pagode");
+        requestMap.put("contatos", "Número do celular");
+        requestMap.put("codigo", "testando1");
+        requestMap.put("interesse", "Banda");
+
+        FakeRequest fakeRequest = new FakeRequest().withFormUrlEncodedBody(requestMap);
+        Result resultPost = callAction(controllers.routes.ref.Application.novoAnuncio(), fakeRequest);
+        assertThat(status(resultPost)).isEqualTo(OK);
+
+        Result resultGet = callAction(controllers.routes.ref.Application.anuncios(), new FakeRequest());
+        assertThat(status(resultGet)).isEqualTo(OK);
+        assertThat(contentType(resultGet)).isEqualTo("text/html");
+        assertThat(contentAsString(resultGet)).contains("Testando");
+    }
+
+    @Test
+    public void deveFinalizarAnuncio() {
+        List<Anuncio> anuncios = DAO.findAllByClass(Anuncio.class);
+        Anuncio anuncioTest = anuncios.get(0);
+
+        Map<String, String> requestMap = new HashMap<>();
+        requestMap.put("finalizar", "teste1");
+        requestMap.put("encontrouParceiros", "Sim");
+
+        FakeRequest fakeRequest = new FakeRequest().withFormUrlEncodedBody(requestMap);
+        Result resultPost = callAction(controllers.routes.ref.Application.finalizaAnuncio("teste1", anuncioTest.getId()), fakeRequest);
+        assertThat(status(resultPost)).isEqualTo(OK);
+        assertThat(contentAsString(resultPost)).contains("<h4>@finalizados Anúncios finalizados</h4>");
+        assertThat(contentAsString(resultPost)).doesNotContain("<h4 class='anuncioTitulo'>Teste 1</h4>");
+    }
+
+    @Test
+    public void deveFazerPerguntaEResposta() {
+        List<Anuncio> anuncios = DAO.findAllByClass(Anuncio.class);
+        Anuncio anuncioTest = anuncios.get(0);
+
+        Map<String, String> requestMap = new HashMap<>();
+        requestMap.put("pergunta", "Pergunta de teste");
+
+        FakeRequest fakeRequest = new FakeRequest().withFormUrlEncodedBody(requestMap);
+        Result resultPost = callAction(controllers.routes.ref.Application.fazerPergunta(anuncioTest.getId()), fakeRequest);
+        assertThat(status(resultPost)).isEqualTo(OK);
+        assertThat(contentAsString(resultPost)).contains("Pergunta de teste");
+
+        Conversa conversa = anuncioTest.getConversas().get(0);
+
+        Map<String, String> requestMap2 = new HashMap<>();
+        requestMap2.put("resposta", "Resposta de teste");
+        requestMap2.put("codigo", "teste1");
+
+        FakeRequest fakeRequest2 = new FakeRequest().withFormUrlEncodedBody(requestMap2);
+        Result resultPost2 = callAction(controllers.routes.ref.Application.responderPergunta(conversa.getId(), anuncioTest.getId()), fakeRequest2);
+        assertThat(status(resultPost2)).isEqualTo(OK);
+        assertThat(contentAsString(resultPost2)).contains("Resposta de teste");
+    }
+
+    @After
+    public void tearDown() {
+        em.getTransaction().commit();
+        JPA.bindForCurrentThread(null);
+        em.close();
+    }
+}
